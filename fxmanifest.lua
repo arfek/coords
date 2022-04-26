892ea36a9c96c4efafa757491795bdf870d0fb9c
Arthur Felipe Kraemer <56006418+arfek@users.noreply.github.com>
2022-04-26 13:45:29 -0300

    Add: fxmanifest.lua file
    Base commit.

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games {'gta5' }
+
+author 'Arthur Kraemer <giovanella.arthur@gmail.com>'
+description 'Coordinates Command'
+version '1.0'
+
+
+client_scripts {
+    'client.lua'
+}
